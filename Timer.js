ccab74ad80eef19671d506939819d14edadfd040
break7533 <ll61295@gmail.com>
2019-04-29 20:09:23 +0100

    2 files modified

--- Timer.js
+++ Timer.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 function Timer() {
@@ -6,19 +6,19 @@ function Timer() {
 
   useEffect(() => {
 
-  const tick = () => {
-    setSeconds(sec => sec + 1);
-  }
+    const tick = () => {
+      setSeconds(sec => sec + 1);
+    }
 
-  const interval = setInterval(() => tick(), 1000);
-  return () => clearInterval(interval);
+    const interval = setInterval(() => tick(), 1000);
+    return () => clearInterval(interval);
   }, []);
 
-    return (
-      <div>
-        Seconds: {seconds}
-      </div>
-    );
-  }
+  return (
+    <div>
+      Seconds: {seconds}
+    </div>
+  );
+}
 
 export default Timer;
--- ToDo.js
+++ ToDo.js
@@ -1,66 +1,37 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
-
-/*
-class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: [], text: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({ text: e.target.value });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    if (!this.state.text.length) {
-      return;
-    }
-    const newItem = {
-      text: this.state.text,
-      id: Date.now()
-    };
-    this.setState(state => ({
-      items: state.items.concat(newItem),
-      text: ''
-    }));
-  }
-}
-*/
 
 function TodoApp() {
 
-const [items,setItems] = useState([]);
-const [text,setText] = useState('');
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState('');
+
   return (
-      <div>
-        <h3>TODO</h3>
-        <TodoList items={items} />
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="new-todo">
-            What needs to be done?
+    <div>
+      <h3>TODO</h3>
+      <TodoList items={items} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="new-todo">
+          What needs to be done?
           </label>
-          <input
-            id="new-todo"
-            onChange={handleChange}
-            value={text}
-          />
-          <button>
-            Add #{items.length + 1}
-          </button>
-        </form>
-      </div>
-    );
+        <input
+          id="new-todo"
+          onChange={handleChange}
+          value={text}
+        />
+        <button>
+          Add #{items.length + 1}
+        </button>
+      </form>
+    </div>
+  );
 
-  function handleChange(e){
+  function handleChange(e) {
     setText(e.target.value);
-}
+  }
 
-function handleSubmit(e){
-  e.preventDefault();
+  function handleSubmit(e) {
+    e.preventDefault();
     if (!text.length) {
       return;
     }
@@ -71,17 +42,17 @@ function handleSubmit(e){
 
     setItems(items => items.concat(newItem));
     setText('');
-}
+  }
 }
 
 function TodoList({ items }) {
-    return (
-      <ul>
-        {items.map(item => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </ul>
-    );
+  return (
+    <ul>
+      {items.map(item => (
+        <li key={item.id}>{item.text}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default TodoApp;
